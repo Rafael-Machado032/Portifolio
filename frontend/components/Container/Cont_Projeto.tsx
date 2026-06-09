48f1c8e28c7158b5f0b4567ea92138caf930bfc6
Rafael-Machado032 <rafaelmfds@gmail.com>
2026-06-09 11:47:59 -0300

    a

--- frontend/components/Container/Cont_Projeto.tsx
+++ frontend/components/Container/Cont_Projeto.tsx
@@ -29,11 +29,11 @@ export default function Cont_Projeto({ projetoDados }: { projetoDados: Projeto }
     }, [projetoDados.descricao]);
 
     return (
-        <div className="flex flex-col max-w-sm min-h-[430.2px] rounded-2xl text-white bg-gray-500 dark:bg-[#222222] border border-[#00f1fe00] hover:border-[#00f2fe] dark:hover:border-[#00f2fe] hover:shadow-[0_0_20px_rgba(0,133,140,0.6)] dark:hover:shadow-[0_0_20px_rgba(0,242,242,0.6)] hover:scale-105 transition-all duration-300">
+        <div className="flex flex-col max-w-85 justify-center items-center min-h-[430.2px] rounded-2xl text-white bg-gray-500 dark:bg-[#222222] border border-[#00f1fe00] hover:border-[#00f2fe] dark:hover:border-[#00f2fe] hover:shadow-[0_0_20px_rgba(0,133,140,0.6)] dark:hover:shadow-[0_0_20px_rgba(0,242,242,0.6)] hover:scale-105 transition-all duration-300">
             <div className="relative w-full aspect-video">
                 <NextImage className="rounded-t-2xl" src={projetoDados.layout_url} alt="Capa" fill unoptimized priority />
             </div>
-            <div className="flex flex-col justify-between w-80 gap-2 p-4 flex-1">
+            <div className="flex flex-col justify-between items-center gap-2 p-4 flex-1">
                 <div>
                     <span className="text-[#00f2ff] bg-[#00424572] px-4 pb-1 rounded-4xl">{projetoDados.tecnologia.split(',').map(t => t.trim()).join(' + ')}</span>
                     <h3 className="text-xl mt-2">{projetoDados?.titulo}</h3>
@@ -49,7 +49,7 @@ export default function Cont_Projeto({ projetoDados }: { projetoDados: Projeto }
                         </button>
                     )}
                 </div>
-                <div className="w-full flex justify-center items-center gap-1 mt-6">
+                <div className="flex justify-center items-center gap-1 mt-6">
                     <Demo href={projetoDados?.demonstracao_url} />
                     {projetoDados.github_url &&
                         <GithubProjeto href={projetoDados?.github_url} />
